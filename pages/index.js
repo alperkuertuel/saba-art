3a3e9c53cb4f25d115fd3a0cd9f1d06680f43c86
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-14 21:54:53 +0200

    lift the state up and use local storage state add chance to see screen behavoir for longer names

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import ArtPiecesPreview from "@/components/ArtPiecesPreview/ArtPiecesPreview";
 import Header from "@/components/Header/Header";
-import artPiecesData from "@/db/data";
-import { useState } from "react";
 
-export default function HomePage() {
-  const [artPieces, setArtPieces] = useState(artPiecesData);
+export default function HomePage({ artPieces }) {
   return (
     <>
       <Header />
