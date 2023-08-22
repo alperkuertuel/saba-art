864957d19f39bb81d79532be0224bb4fe40703fa
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-22 11:33:56 +0200

    create filter to remove all duplicates

--- components/CategoryFilter/CategoryFilter.js
+++ components/CategoryFilter/CategoryFilter.js
@@ -1,6 +1,11 @@
 export default function CategoryFilter({ artPieces }) {
   const allCategories = artPieces.map((piece) => piece.category);
-  console.log(allCategories);
+
+  const withoutDuplicates = allCategories.filter(
+    (item, index) => allCategories.indexOf(item) === index
+  );
+
+  console.log(withoutDuplicates);
   return (
     <p>
       <button>Filter</button>
