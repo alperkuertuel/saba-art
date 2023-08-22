be6cd1c7b56558bebff08dd11b982ee1d2b6ee64
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-22 11:35:03 +0200

    add other data to see if the filter works

--- db/data.js
+++ db/data.js
@@ -8,8 +8,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Abstract",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2019/04/26/17/47/color-4158152_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2019/04/26/17/47/color-4158152_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -23,8 +22,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -37,8 +35,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Landscape",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/02/24/18/15/andreas-achenbach-85762_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/02/24/18/15/andreas-achenbach-85762_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -51,8 +48,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Portrait",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/01/05/21/02/art-74050_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/01/05/21/02/art-74050_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -65,8 +61,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Landscape",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2018/09/20/17/01/birch-3691340_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2018/09/20/17/01/birch-3691340_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -79,8 +74,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/02/24/18/32/sebastian-pether-85797_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/02/24/18/32/sebastian-pether-85797_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -93,8 +87,20 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/03/03/19/46/george-durrie-89656_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/03/03/19/46/george-durrie-89656_1280.jpg",
+    date: "2021",
+    widthReal: "100",
+  },
+  {
+    id: "7",
+    slug: "nordic-snow-hut",
+    name: "Nordic Snow Hut",
+    date: "2011",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    category: "Test",
+    technique: "Oil",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/03/03/19/46/george-durrie-89656_1280.jpg",
     date: "2021",
     widthReal: "100",
   },
@@ -108,8 +114,7 @@ const artPiecesData = [
 
     category: "Abstract",
     technique: "Acryl",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2017/06/01/02/17/abstract-2362258_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2017/06/01/02/17/abstract-2362258_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -122,8 +127,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Acryl",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2014/05/11/17/30/painting-342116_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2014/05/11/17/30/painting-342116_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -136,8 +140,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Abstract",
     technique: "Acryl",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2020/03/09/10/23/color-4915201_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2020/03/09/10/23/color-4915201_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -150,8 +153,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Abstract",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2018/02/04/09/09/art-3129359_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2018/02/04/09/09/art-3129359_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -164,8 +166,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Abstract",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2022/10/22/01/56/abstract-7538251_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2022/10/22/01/56/abstract-7538251_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -178,8 +179,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Landscape",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/02/19/18/10/robert-duncanson-83429_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/02/19/18/10/robert-duncanson-83429_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -192,8 +192,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Landscape",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/02/24/18/32/ramon-alsina-85796_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/02/24/18/32/ramon-alsina-85796_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -206,8 +205,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/02/14/03/57/christen-kobk-81509_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/02/14/03/57/christen-kobk-81509_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -220,8 +218,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/03/09/19/14/ivan-alvazovsky-91987_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/03/09/19/14/ivan-alvazovsky-91987_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -234,8 +231,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Impression",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/03/08/15/51/johan-dahl-91590_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/03/08/15/51/johan-dahl-91590_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
@@ -248,8 +244,7 @@ const artPiecesData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     category: "Landscape",
     technique: "Oil",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/03/02/22/21/caspar-friedrich-89414_1280.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2013/03/02/22/21/caspar-friedrich-89414_1280.jpg",
     heightReal: "100",
     widthReal: "100",
   },
