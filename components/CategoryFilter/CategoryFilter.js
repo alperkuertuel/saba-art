3cbc89e03d6b3b9de6f414f42d3da8c5333971e6
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-22 12:04:55 +0200

    delete all and start from beginning

--- components/CategoryFilter/CategoryFilter.js
+++ components/CategoryFilter/CategoryFilter.js
@@ -3,17 +3,7 @@ import { uid } from "uid";
 
 export default function CategoryFilter({ artPieces }) {
   const allCategories = artPieces.map((piece) => piece.category);
-  console.log(allCategories);
 
-  const withoutDuplicates = allCategories.filter(
-    (item, index) => allCategories.indexOf(item) != index
-  );
-
-  const duplicated = [1, 2, 3, 2, 3, 4, 3, 5];
-  const uniqSet = new Set(duplicated);
-  console.log([...uniqSet]); // Should be [1, 2, 3, 4, 5]
-
-  console.log(withoutDuplicates);
   return (
     <StyledCategoryFilter>
       {withoutDuplicates.map((category) => (
