a11ccebe11fe0f4f6a2ed3c6baf8605454a8d69b
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-30 20:24:31 +0200

    finish webmanifest with missing service worker

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function HomePage({
       <Head>
         <title>ArtistName - pictures are memories</title>
         <meta name="description" content="the gallery of ArtistName" />
+        <meta name="theme-color" content="white" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
