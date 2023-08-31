a06fc23b9ced03019f2c11c5f3c286e1610db61b
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-31 22:02:00 +0200

    delete not protected commented post route

--- pages/api/index.js
+++ pages/api/index.js
@@ -6,36 +6,11 @@ import { getServerSession } from "next-auth/next";
 export default async function handler(request, response) {
   await dbConnect();
 
-  // switch (request.method) {
-  //   case "GET":
-  //     try {
-  //       const session = await getServerSession(request, response, authOptions);
-
-  //       if (!session) {
-  //         return response.status(401).json({ message: "Unauthorized" });
-  //       }
-
-  //       const artPieces = await ArtPiece.find();
-  //       if (!artPieces) {
-  //         return response.status(404).json({ message: "Art piece not found!" });
-  //       }
-
-  //       if (session.user.email !== artPieces.author) {
-  //         return response.status(401).json({ message: "Unauthorized, wrong!" });
-  //       }
-  //       return response.status(200).json(artPieces);
-  //     } catch (error) {
-  //       console.log(error);
-  //       return response.status(500).json({ error: "Error!" });
-  //     }
-  //   default:
-  //     response.status(500).json({ error: "Method is not allowed" });
-  // }
-
   if (request.method === "GET") {
     const artPieces = await ArtPiece.find();
     return response.status(200).json(artPieces);
   }
+
   switch (request.method) {
     case "POST":
       try {
@@ -57,16 +32,3 @@ export default async function handler(request, response) {
       response.status(405).json({ error: "Method not allowed" });
   }
 }
-
-// if (request.method === "POST") {
-//   try {
-//     const newArtPieceData = request.body;
-//     await ArtPiece.create(newArtPieceData);
-
-//     response.status(201).json({ status: "Art piece created" });
-//   } catch (error) {
-//     response
-//       .status(400)
-//       .json({ error: "Something went wrong during the upload of your art piece" });
-//   }
-// }
