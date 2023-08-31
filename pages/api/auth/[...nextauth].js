51fe70a23e592a7ed0ec97d27706365c0bd84143
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-31 15:13:12 +0200

    add fake username password login for preview

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 async function getUserRoleFromDatabase(email) {
   if (email === "alperkuertuel@hotmail.de") {
@@ -8,13 +9,39 @@ async function getUserRoleFromDatabase(email) {
   return "viewer";
 }
 
+const fakeLogin = CredentialsProvider({
+  name: "Credentials",
+  credentials: {
+    username: { label: "Username", type: "text", placeholder: "fish" },
+    password: { label: "Password", type: "password" },
+  },
+  // and adding a fake authorization with static username and password:
+  async authorize(credentials) {
+    if (credentials.username === "artist" && credentials.password === "art-gallery") {
+      return {
+        id: "1",
+        name: "artist",
+        email: "artist@github.com",
+      };
+    } else {
+      return null;
+    }
+  },
+});
+
+const providers =
+  process.env.VERCEL_ENV === "preview"
+    ? [fakeLogin]
+    : [
+        GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
+        // ...add more providers here
+      ];
+
 export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
+  providers,
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
