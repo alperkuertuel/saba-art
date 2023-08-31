241a2391297b02af8b93257e58ddb32974275e06
Alper Kürtül <alperkuertuel@hotmail.de>
2023-08-31 21:29:17 +0200

    secure post methode first only with an existing session

--- pages/api/index.js
+++ pages/api/index.js
@@ -37,16 +37,37 @@ export default async function handler(request, response) {
     return response.status(200).json(artPieces);
   }
 
-  if (request.method === "POST") {
-    try {
-      const newArtPieceData = request.body;
-      await ArtPiece.create(newArtPieceData);
+  switch (request.method) {
+    case "POST":
+      try {
+        const session = await getServerSession(request, response, authOptions);
+        if (!session) {
+          return response.status(401).json({
+            message: "Status 401: You are not authorized! Only logged in users can add pictures!",
+          });
+        }
 
-      response.status(201).json({ status: "Art piece created" });
-    } catch (error) {
-      response
-        .status(400)
-        .json({ error: "Something went wrong during the upload of your art piece" });
-    }
+        const newArtPieceData = request.body;
+        await ArtPiece.create(newArtPieceData);
+        return response.status(201).json(newArtPieceData);
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({ error: "Error!" });
+      }
+    default:
+      response.status(405).json({ error: "Error!" });
   }
 }
+
+// if (request.method === "POST") {
+//   try {
+//     const newArtPieceData = request.body;
+//     await ArtPiece.create(newArtPieceData);
+
+//     response.status(201).json({ status: "Art piece created" });
+//   } catch (error) {
+//     response
+//       .status(400)
+//       .json({ error: "Something went wrong during the upload of your art piece" });
+//   }
+// }
