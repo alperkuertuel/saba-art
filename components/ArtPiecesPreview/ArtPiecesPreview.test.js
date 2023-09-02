abf49e3a0fb9113456bf9c39491bc5f7ba0b1f52
Alper Kürtül <alperkuertuel@hotmail.de>
2023-09-02 17:12:28 +0200

    fix failing test / failure due to new prop

--- components/ArtPiecesPreview/ArtPiecesPreview.test.js
+++ components/ArtPiecesPreview/ArtPiecesPreview.test.js
@@ -4,7 +4,7 @@ import ArtPiecesPreview from "./ArtPiecesPreview";
 import artPiecesData from "@/db/BackUp/data";
 
 test("Each art piece's alt text is displayed", () => {
-  render(<ArtPiecesPreview artPieces={artPiecesData} />);
+  render(<ArtPiecesPreview filteredCategory={artPiecesData} />);
   artPiecesData.forEach((piece) => {
     const imageElement = screen.getByAltText(piece.name);
     expect(imageElement).toHaveAttribute("alt", piece.name);
@@ -13,7 +13,7 @@ test("Each art piece's alt text is displayed", () => {
 });
 
 test("Each art piece's name is displayed", () => {
-  render(<ArtPiecesPreview artPieces={artPiecesData} />);
+  render(<ArtPiecesPreview filteredCategory={artPiecesData} />);
   artPiecesData.forEach((piece) => {
     const nameElement = screen.getByText(piece.name);
     expect(nameElement).toBeInTheDocument(piece.name);
@@ -21,7 +21,7 @@ test("Each art piece's name is displayed", () => {
 });
 
 test("Each art piece's date is displayed", () => {
-  render(<ArtPiecesPreview artPieces={artPiecesData} />);
+  render(<ArtPiecesPreview filteredCategory={artPiecesData} />);
   artPiecesData.forEach((piece) => {
     const dateElement = screen.getByRole("generic");
     expect(dateElement).toBeInTheDocument(piece.date);
