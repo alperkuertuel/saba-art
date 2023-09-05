f4197e6fd9e59a8ddce5372fb4814eac9c43941c
Alper Kürtül <alperkuertuel@hotmail.de>
2023-09-05 15:19:46 +0200

    set states for Cropper

--- components/ImageCropDialog/ImageCropDialog.js
+++ components/ImageCropDialog/ImageCropDialog.js
@@ -1,3 +1,7 @@
+import { styled } from "styled-components";
+import Cropper from "react-easy-crop";
+import { useState } from "react";
+
 const aspectRatios = [
   { value: 4 / 3, text: "4/3" },
   { value: 16 / 9, text: "16/9" },
@@ -14,5 +18,23 @@ export default function ImageCropDialog({ fileImageUrl, cropInit, zoomInit, aspe
   if (aspectInit == null) {
     aspectInit = aspectRatios[0];
   }
-  return <span>Crop</span>;
+  const [zoom, setZoom] = useState(zoomInit);
+  const [crop, setCrop] = useState(cropInit);
+  const [aspect, setAspect] = useState(aspectInit);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  return (
+    <BackDrop>
+      <CropContainer>
+        <Cropper />
+      </CropContainer>
+      <Controls></Controls>
+    </BackDrop>
+  );
 }
+
+const BackDrop = styled.span``;
+
+const CropContainer = styled.span``;
+
+const Controls = styled.span``;
