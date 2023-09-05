060f3ac7b8b579d4bc1365aaff88e90fdd659b4f
Alper Kürtül <alperkuertuel@hotmail.de>
2023-09-05 12:29:59 +0200

    change shortname and set theme-color

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="theme-color" content="white" />
+          <meta name="theme-color" content="#d9cbc4" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
