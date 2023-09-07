3080e32faa74ba308a371127797d767e5afb2481
Alper Kürtül <alperkuertuel@hotmail.de>
2023-09-07 20:04:54 +0200

    delete word break

--- components/AdminArtPiecesList/AdminArtPiecesList.js
+++ components/AdminArtPiecesList/AdminArtPiecesList.js
@@ -95,7 +95,6 @@ const StyledItem = styled.li`
   border-radius: 5px;
   box-shadow: var(--box-shadow);
   background-color: var(--box-color);
-  word-break: break-all;
 `;
 
 const StyledLink = styled(Link)`
