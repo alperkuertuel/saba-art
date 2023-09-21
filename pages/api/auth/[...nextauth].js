354fc2f84176311eb80860be7730a2352301563b
alper92 <alperkuertuel@hotmail.de>
2023-09-21 11:22:49 +0300

    delete github provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -36,16 +36,6 @@ const fakeLogin = CredentialsProvider({
 });
 
 const providers = [
-  GithubProvider({
-    clientId:
-      process.env.NEXTAUTH_URL === "http://localhost:3000/"
-        ? process.env.GITHUB_ID_LOCALHOST
-        : process.env.GITHUB_ID,
-    clientSecret:
-      process.env.NEXTAUTH_URL === "http://localhost:3000/"
-        ? process.env.GITHUB_SECRET_LOCALHOST
-        : process.env.GITHUB_SECRET,
-  }),
   GoogleProvider({
     clientId:
       process.env.NEXTAUTH_URL === "http://localhost:3000/"
