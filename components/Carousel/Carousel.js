1b2376b65aebe03e664e17678fb31169ae093a51
alper92 <alperkuertuel@hotmail.de>
2023-09-27 15:51:59 +0300

    create link to details page for carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,14 +2,17 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import styled from "styled-components";
+import Link from "next/link";
 
 export default function ImageCarousel({ filteredCategory }) {
   return (
     <StyledCarousel showIndicators={false} dynamicHeight={true} showThumbs={false}>
-      {filteredCategory.map(({ imageUrl, name, _id }) => (
-        <div key={_id}>
-          <StyledImage src={imageUrl} alt={name} aria-label={name} width={1000} height={1000} />
-        </div>
+      {filteredCategory.map(({ imageUrl, name, _id, slug }) => (
+        <Link key={_id} href={`/art-pieces/${slug}`}>
+          <div>
+            <StyledImage src={imageUrl} alt={name} aria-label={name} width={1000} height={1000} />
+          </div>
+        </Link>
       ))}
     </StyledCarousel>
   );
