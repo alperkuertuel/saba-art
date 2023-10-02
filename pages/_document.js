e8868a704bf19e9d2229de656cc421c2fd1cff8b
alper92 <alperkuertuel@hotmail.de>
2023-10-02 15:06:22 +0300

    add meta tag for insecure requests

--- pages/_document.js
+++ pages/_document.js
@@ -28,6 +28,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content="#d9cbc4" />
           <meta name="google" content="notranslate"></meta>
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
