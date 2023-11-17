4b045223ee1da688c861271b9c21e0d9e0dfeb88
abc <abc@example.com>
2023-11-17 19:36:49 +0100

    add new articles to slider

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -8,6 +8,27 @@ export default function ImageCarousel() {
     <>
       <h3>Presseartikel über Saba:</h3>
       <Carousel showIndicators={false} dynamicHeight={true} showThumbs={false} infiniteLoop={true}>
+      <a
+          href={`/documents/auszug_allgemein.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/auszug_allgemein.png"
+              alt="Auszug meines Gästebuchs"
+              aria-label="Auszug meines Gästebuchs"
+              priority={true}
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                Auszug aus meinem Gästebuch, welches zu allen meinen Ausstellungen ausliegt. Das Zitat <q>Erfolg ist so einsam und eiskalt wie der Nordpol</q> deutet darauf hin, dass der Weg zum Erfolg oft von Isolation und emotionaler Kälte begleitet wird, da die Herausforderungen und Opfer, die für den Erfolg erforderlich sind, eine persönliche Distanz schaffen können.
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
         <a
           href={`/documents/vom_bosporus_zum_bodensee_20-03-1986.pdf`}
           target="_blank"
@@ -44,7 +65,47 @@ export default function ImageCarousel() {
             />
             <StyledLegend>
               <p className="legend">
-                <q>An den Ufern zweier Meere</q> - 29.06.1989
+                Erscheinen am 29.06.1989 in der Regionalzeitung Kreisbote. Dem Autor sei der Namensirrtum verziehen.
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
+        <a
+          href={`/documents/aus_dem_kulturleben_1990.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/aus_dem_kulturleben.png"
+              alt="Aus dem Kulturleben"
+              aria-label="Aus dem Kulturleben"
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                <q>Aus dem Kulturleben</q> - 1990
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
+        <a
+          href={`/documents/suedliches_temperament_09-11-1995.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/suedliches_temperament_09-11-1995.png"
+              alt="Südliches Temperament begegnet dem Allgäu"
+              aria-label="Südliches Temperament begegnet dem Allgäu"
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                <q>Es war mir eine Freude mich mit den genannten Künstlerinnen aus Spanien und Peru auszutauschen.</q> - 09.11.1995
               </p>
             </StyledLegend>
           </div>
@@ -85,6 +146,69 @@ export default function ImageCarousel() {
             <StyledLegend>
               <p className="legend">
                 <q>Peruanische Geschichten</q> - 17.12.2003
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
+        <a
+          href={`/documents/stille_motive_28-10-2006.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/stille_motive.png"
+              alt="Stille Motive"
+              aria-label="Stille Motive"
+              priority={true}
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                Erscheinen 28.10.2006 in der AZ Kempten Nr. 249 unter der Rubrik Kultur am Ort. Ein Dank geht an die Stadtverwaltung Kempten, welche die Austellung in Kollaboration ermöglichte.
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
+        <a
+          href={`/documents/vielfaeltige_kunstwelten.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/vielfaeltige_kunstwelten.png"
+              alt="Vielfältige Kunstwelten"
+              aria-label="Vielfältige Kunstwelten"
+              priority={true}
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                Sieben Künstler kommen für ein Wochenende im Thingers-Bürgersaal zusammen.
+              </p>
+            </StyledLegend>
+          </div>
+        </a>
+        <a
+          href={`/documents/kunstwelten_sind_zu_gast_2012.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div style={{ position: "relative" }}>
+            <StyledImage
+              src="/documents/kunstwelten_sind_zu_gast_2012.png"
+              alt="Kunstwelten sind zu Gast"
+              aria-label="Kunstwelten sind zu Gast"
+              priority={true}
+              width={1000}
+              height={1000}
+            />
+            <StyledLegend>
+              <p className="legend">
+                Eine der schönsten Kollektivausstellungen im Bürgertreff Thingers - 2012
               </p>
             </StyledLegend>
           </div>
