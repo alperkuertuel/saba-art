dfbe99c5b45ef9af7267a96532a3796782426a78
abc <abc@example.com>
2023-11-18 15:56:02 +0100

    fix slider links

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,6 +2,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import styled from "styled-components";
+import Link from "next/link";
 
 export default function ImageCarousel() {
   return (
@@ -15,7 +16,7 @@ export default function ImageCarousel() {
           infiniteLoop={true}
           swipeScrollTolerance={100}
         >
-          <a href={`/documents/auszug_allgemein.pdf`} target="_blank" rel="noopener noreferrer">
+          <div>
             <StyledImage
               src="/documents/auszug_allgemein.png"
               alt="Auszug meines Gästebuchs"
@@ -24,19 +25,20 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Auszug aus meinem Gästebuch, welches zu allen meinen Ausstellungen ausliegt. Das Zitat{" "}
-              <q>Erfolg ist so einsam und eiskalt wie der Nordpol</q> deutet darauf hin, dass der
-              Weg zum Erfolg oft von Isolation und emotionaler Kälte begleitet wird, da die
-              Herausforderungen und Opfer, die für den Erfolg erforderlich sind, eine persönliche
-              Distanz schaffen können.
-            </StyledLegend>
-          </a>
-          <a
-            href={`/documents/vom_bosporus_zum_bodensee_20-03-1986.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/auszug_allgemein.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>
+                Das Zitat <q>Erfolg ist so einsam und eiskalt wie der Nordpol</q> aus meinem
+                Ausstellungs-Gästebuch betont, dass der Weg zum Erfolg oft von Isolation und
+                emotionaler Kälte begleitet wird, bedingt durch die erforderlichen Herausforderungen
+                und Opfer.
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/vom_bosporus_zum_bodensee.png"
               alt="Vom Bospurus zum Bodensee"
@@ -45,13 +47,15 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>Vom Bospurus zum Bodensee - 20.03.1986</StyledLegend>
-          </a>
-          <a
-            href={`/documents/an_den_ufern_zweier_meere_29-06-1989.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/vom_bosporus_zum_bodensee_20-03-1986.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>Vom Bospurus zum Bodensee - 20.03.1986</StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/an_den_ufern_zweier_meere.png"
               alt="An den Ufern zweier Meere"
@@ -59,16 +63,18 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Erscheinen am 29.06.1989 in der Regionalzeitung Kreisbote. Dem Autor sei der
-              Namensirrtum verziehen.
-            </StyledLegend>
-          </a>
-          <a
-            href={`/documents/aus_dem_kulturleben_1990.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/an_den_ufern_zweier_meere_29-06-1989.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>
+                Erscheinen am 29.06.1989 in der Regionalzeitung Kreisbote. Dem Autor sei der
+                Namensirrtum verziehen.
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/aus_dem_kulturleben.png"
               alt="Aus dem Kulturleben"
@@ -76,13 +82,15 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>Aus dem Kulturleben - 1990</StyledLegend>
-          </a>
-          <a
-            href={`/documents/suedliches_temperament_09-11-1995.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/aus_dem_kulturleben_1990.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>Aus dem Kulturleben - 1990</StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/suedliches_temperament_09-11-1995.png"
               alt="Südliches Temperament begegnet dem Allgäu"
@@ -90,16 +98,18 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Es war mir eine Freude mich mit den genannten Künstlerinnen aus Spanien und Peru
-              auszutauschen. - 09.11.1995
-            </StyledLegend>
-          </a>
-          <a
-            href={`/documents/kultur_am_ort_25-02-1998.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/suedliches_temperament_09-11-1995.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>
+                Es war mir eine Freude mich mit den genannten Künstlerinnen aus Spanien und Peru
+                auszutauschen. - 09.11.1995
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/kultur_am_ort.png"
               alt="Kultur am Ort"
@@ -107,14 +117,15 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>Kultur am Ort - 25.02.1998</StyledLegend>
-          </a>
-          <a
-            href={`/documents/peruanische_geschichten_17-12-03.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
+            <StyledCarouselLinkWrapper
+              href={`/documents/kultur_am_ort_25-02-1998.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>Kultur am Ort - 25.02.1998</StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/peruanische_geschichten.png"
               alt="Peruanische Geschichten"
@@ -122,13 +133,15 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>Peruanische Geschichten - 17.12.2003</StyledLegend>
-          </a>
-          <a
-            href={`/documents/stille_motive_28-10-2006.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/peruanische_geschichten_17-12-03.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>Peruanische Geschichten - 17.12.2003</StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/stille_motive.png"
               alt="Stille Motive"
@@ -137,17 +150,19 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Erscheinen 28.10.2006 in der AZ Kempten Nr. 249 unter der Rubrik Kultur am Ort. Ein
-              Dank geht an die Stadtverwaltung Kempten, welche die Austellung in Kollaboration
-              ermöglichte.
-            </StyledLegend>
-          </a>
-          <a
-            href={`/documents/vielfaeltige_kunstwelten.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/stille_motive_28-10-2006.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>
+                Erscheinen 28.10.2006 in der AZ Kempten Nr. 249 unter der Rubrik Kultur am Ort. Ein
+                Dank geht an die Stadtverwaltung Kempten, welche die Austellung in Kollaboration
+                ermöglichte.
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/vielfaeltige_kunstwelten.png"
               alt="Vielfältige Kunstwelten"
@@ -156,15 +171,17 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Sieben Künstler kommen für ein Wochenende im Thingers-Bürgersaal zusammen.
-            </StyledLegend>
-          </a>
-          <a
-            href={`/documents/kunstwelten_sind_zu_gast_2012.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <StyledCarouselLinkWrapper
+              href={`/documents/vielfaeltige_kunstwelten.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend>
+                Sieben Künstler kommen für ein Wochenende im Thingers-Bürgersaal zusammen.
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
+          <div>
             <StyledImage
               src="/documents/kunstwelten_sind_zu_gast_2012.png"
               alt="Kunstwelten sind zu Gast"
@@ -173,10 +190,16 @@ export default function ImageCarousel() {
               width={1000}
               height={1000}
             />
-            <StyledLegend>
-              Eine der schönsten Kollektivausstellungen im Bürgertreff Thingers - 2012
-            </StyledLegend>
-          </a>
+            <StyledCarouselLinkWrapper
+              href={`/documents/kunstwelten_sind_zu_gast_2012.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLegend width={1000} height={1000}>
+                Eine der schönsten Kollektivausstellungen im Bürgertreff Thingers - 2012
+              </StyledLegend>
+            </StyledCarouselLinkWrapper>
+          </div>
         </Carousel>
       </CarouselWrapper>
     </>
@@ -186,19 +209,29 @@ export default function ImageCarousel() {
 const CarouselWrapper = styled.div`
   width: 85%;
   margin: 2rem auto;
+  position: relative;
+`;
+
+const StyledCarouselLinkWrapper = styled(Link)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
 `;
 
 const StyledImage = styled(Image)`
   position: relative;
   object-fit: cover;
   width: 100%;
-  height: 100%;
-  opacity: 0.6;
+  height: 60vh;
+  opacity: 0.4;
   border-radius: 5px;
 `;
 
 const StyledLegend = styled.span`
   position: absolute;
+  text-align: justify;
   color: var(--font-color);
   background-color: var(--primary-color);
   display: block;
@@ -208,4 +241,5 @@ const StyledLegend = styled.span`
   right: 0px;
   width: 100%;
   height: auto;
+  opacity: 0.9;
 `;
