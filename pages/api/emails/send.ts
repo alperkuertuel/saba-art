37a1e2d164727f2f18ef815f68806270cc03aca1
Alper Kürtül <83625276+alperkuertuel@users.noreply.github.com>
2024-10-22 00:15:02 +0200

    Update send.ts

--- pages/api/emails/send.ts
+++ pages/api/emails/send.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 
-import KontaktEmail from '@/Emails/KontaktEmail';
+import KontaktEmail from '@/emails/KontaktEmail';
 import { KontaktFormularProperties } from '@/Kontakt/KontaktFormular';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
